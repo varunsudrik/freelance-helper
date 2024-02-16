360c72d98e33d34d21a06c3b9986f6a937d1893c
Varun Sudrik <varunsudrik99@gmail.com>
2024-02-16 11:13:00 +0530

    structure update

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ export {
     IReduxLogout,
     IAuthResponse,
     IAuthUser,
-} from './interfaces/auth.interface';
-export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
+} from './auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './buyer.interface';
 export {
     IConversationDocument,
     IMessageDocument,
@@ -23,7 +23,7 @@ export {
     IChatSellerProps,
     IChatBuyerProps,
     IChatMessageProps,
-} from './interfaces/chat.interface';
+} from './chat.interface';
 export {
     GigType,
     ICreateGig,
@@ -35,7 +35,7 @@ export {
     IGigViewReviewsProps,
     IGigInfo,
     IGigTopProps,
-} from './interfaces/gig.interface';
+} from './gig.interface';
 export {
     IOrderNotifcation,
     IOffer,
@@ -45,14 +45,14 @@ export {
     IOrderReview,
     IOrderMessage,
     IOrderDocument,
-} from './interfaces/order.interface';
+} from './order.interface';
 export {
     IReviewMessageDetails,
     IRatingTypes,
     IRatingCategories,
     IRatingCategoryItem,
     IReviewDocument,
-} from './interfaces/review.interface';
+} from './review.interface';
 export {
     ISearchResult,
     IHitsTotal,
@@ -60,7 +60,7 @@ export {
     IQueryString,
     ITerm,
     IPaginateProps,
-} from './interfaces/search.interface';
+} from './search.interface';
 export {
     SellerType,
     ILanguage,
@@ -68,8 +68,8 @@ export {
     IEducation,
     ICertificate,
     ISellerDocument,
-} from './interfaces/seller.interface';
-export { IEmailLocals } from './interfaces/email.interface';
+} from './seller.interface';
+export { IEmailLocals } from './email.interface';
 export { uploads, videoUpload } from './cloudinary-upload';
 export {
     IErrorResponse,
